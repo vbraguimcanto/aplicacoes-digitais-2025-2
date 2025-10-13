12bb88aa58030d96fa684c17dde5178cbe74810a
Victor Hugo Braguim Canto <vbraguimcanto@gmail.com>
2025-10-13 16:59:09 -0300

    feat: adicionando exemplo de interação utilizando SQLAlchemy

--- 5. Bancos de Dados Relacionais/ORMs/app.py
+++ 5. Bancos de Dados Relacionais/ORMs/app.py
@@ -1,5 +1,19 @@
 from sqlalchemy import create_engine
-from models import Base
+from sqlalchemy.orm import sessionmaker
+from models import Base, Usuario
 
-engine = create_engine("sqlite:///meubanco.db")
+engine = create_engine("sqlite:///meubanco.db", echo=True)
 Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+novo_usuario = Usuario(nome="Victor", email="victor@exemplo.com")
+session.add(novo_usuario)
+session.commit()
+
+usuarios = session.query(Usuario).all()
+for usuario in usuarios:
+    print(f"ID: {usuario.id}, Nome: {usuario.nome}, Email: {usuario.email}")
+
+session.close()
